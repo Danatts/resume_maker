e4b5b89f5b247f7d602117b3fde6c354ae9aae98
Daniel Arteta <danielatts97@protonmail.com>
2024-06-25 11:41:58 -0500

    feat: add navbar

--- src/app.tsx
+++ src/app.tsx
@@ -3,11 +3,13 @@ import Form from "~/components/Form";
 import Toolbar from "~/components/Toolbar";
 import Preview from "./components/Preview";
 import "~/styles/index.css";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <MetaProvider>
       <Title>Resume Maker</Title>
+      <Navbar />
       <main>
         <section class="grid grid-cols-2 max-w-[1300px] py-2 mx-auto">
           <Form />
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <nav class="px-10 py-3 w-full bg-violet-400">
+      <p class="text-xl font-bold text-black">RESUME MAKER</p>
+    </nav>
+  )
+}
